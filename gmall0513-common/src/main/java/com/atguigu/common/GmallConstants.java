a34352763f604aa4a80d98d0998c8c715e7d8ee5
lizong <1563566702@qq.com>
2019-10-20 12:41:55 +0800

    Initial commit

---
+++ gmall0513-common/src/main/java/com/atguigu/common/GmallConstants.java
@@ -0,0 +1,7 @@
+package com.atguigu.common;
+
+public class GmallConstants {
+    public static final String KAFKA_STARTUP="STARTUP";
+    public static final String KAFKA_EVENT="GMALLS_EVENT";
+
+}
---
+++ gmall0513-logger/src/test/java/com/atguigu/gmall0513/logger/Gmall0513LoggerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall0513.logger;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall0513LoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
